bf09e0dba98e0925cdbcd207bcf9ef9a1980dc48
Kévin Guenet <guenetkevin@gmail.com>
2025-07-29 23:06:25 +0200

    Installation de Material comme livrairie de composants

--- ng_PetBoarding_app/src/app/app.component.ts
+++ ng_PetBoarding_app/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { CommonModule } from "@angular/common";
 import { HttpClient } from "@angular/common/http";
 import { Component, inject, OnInit } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   templateUrl: "app.component.html",
   styleUrl: "./app.component.scss",
 })
