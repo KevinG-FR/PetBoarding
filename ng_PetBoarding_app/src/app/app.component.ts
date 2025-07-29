9ac7538b30edca351ce8d75b82e9e5f2652f79d9
Kévin Guenet <guenetkevin@gmail.com>
2025-07-29 22:49:40 +0200

    Finalisation du passage en standalone

--- ng_PetBoarding_app/src/app/app.component.ts
+++ ng_PetBoarding_app/src/app/app.component.ts
@@ -1,25 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: 'app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule],
+  templateUrl: "app.component.html",
+  styleUrl: "./app.component.css",
 })
-
-export class AppComponent implements OnInit{  
+export class AppComponent implements OnInit {
   http = inject(HttpClient);
-  title = 'ng-PetBoarding-app';
+  title = "ng-PetBoarding-app";
   data: any;
 
   ngOnInit(): void {
-    this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe({
-      next: response => this.data = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    this.http.get("https://jsonplaceholder.typicode.com/todos/1").subscribe({
+      next: (response) => (this.data = response),
+      error: (error) => console.log(error),
+      complete: () => console.log("Request has completed"),
+    });
   }
 }
