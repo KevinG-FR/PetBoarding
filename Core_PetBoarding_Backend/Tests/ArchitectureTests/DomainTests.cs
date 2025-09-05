7e388155ca01a4ce9ea4bd107b1f169b2ef64a56
Kévin Guenet <guenetkevin@gmail.com>
2025-09-05 23:25:52 +0200

    Ajout de tests d'architecture

--- Core_PetBoarding_Backend/Tests/ArchitectureTests/DomainTests.cs
+++ Core_PetBoarding_Backend/Tests/ArchitectureTests/DomainTests.cs
@@ -5,6 +5,15 @@ using FluentAssertions;
 using NetArchTest.Rules;
 
 using PetBoarding_Domain.Abstractions;
+using PetBoarding_Domain.Addresses;
+using PetBoarding_Domain.Baskets;
+using PetBoarding_Domain.Payments;
+using PetBoarding_Domain.Pets;
+using PetBoarding_Domain.Planning;
+using PetBoarding_Domain.Prestations;
+using PetBoarding_Domain.Primitives;
+using PetBoarding_Domain.Reservations;
+using PetBoarding_Domain.Users;
 
 namespace ArchitectureTests;
 
@@ -18,6 +27,7 @@ public class DomainTests
         var result = Types.InAssembly(DomainAssembly)
         .That()
         .ImplementInterface(typeof(IDomainEvent))
+        .And().AreNotAbstract()
         .Should()
         .BeSealed()
         .GetResult();
@@ -39,23 +49,184 @@ public class DomainTests
     }
 
     [Fact]
-    public void Entities_Should_HavePrivateParameterlessConstructor()
+    public void ConcreteEntities_Should_InheritFromEntityBaseClasses()
     {
-        var entityTypes = Types.InAssembly(DomainAssembly)
-            .That()
-            .Inherit(typeof(Entity<>))
-            .GetTypes();
-
-        var failingTypes = new List<Type>();
-
-        foreach (var entityType in entityTypes)
+        // Teste que toutes les entités concrètes héritent bien d'une des classes de base Entity
+        var concreteEntities = new[]
         {
-            var constructors = entityType.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
+            nameof(User), nameof(Address), nameof(Pet), nameof(Prestation), nameof(Reservation), nameof(ReservationSlot),
+            nameof(Planning), nameof(AvailableSlot), nameof(Basket), nameof(BasketItem), nameof(Payment)
+        };
 
-            if (constructors.Any(c => c.IsPrivate && !c.GetParameters().Any()))
-                failingTypes.Add(entityType);
-        }
+        var result = Types.InAssembly(DomainAssembly)
+        .That()
+        .HaveNameMatching(string.Join("|", concreteEntities.Select(e => $"^{e}$")))
+        .Should()
+        .Inherit(typeof(Entity<>))
+        .GetResult();
 
-        failingTypes.Should().BeEmpty();
+        result.IsSuccessful.Should().BeTrue();
     }
+
+    [Fact]
+    public void Entities_Should_BeInCorrectNamespace()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+        .That()
+        .Inherit(typeof(Entity<>))
+        .Should()
+        .ResideInNamespaceMatching("PetBoarding_Domain\\.[A-Za-z]+$")
+        .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Entities_Should_NotReferenceInfrastructure()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+        .That()
+        .Inherit(typeof(Entity<>))
+        .Should()
+        .NotHaveDependencyOn("PetBoarding_Infrastructure")
+        .And()
+        .NotHaveDependencyOn("PetBoarding_Persistence")
+        .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ConcreteValueObjects_Should_InheritFromValueObject()
+    {
+        // Teste que les Value Objects connus héritent bien de ValueObject
+        var valueObjects = new[]
+        {
+            nameof(Email), nameof(Firstname), nameof(Lastname), nameof(PhoneNumber), 
+            nameof(StreetNumber), nameof(StreetName), nameof(City), nameof(PostalCode), 
+            nameof(Country), nameof(Complement), nameof(EmergencyContact)
+        };
+
+        var result = Types.InAssembly(DomainAssembly)
+        .That()
+        .HaveNameMatching(string.Join("|", valueObjects.Select(vo => $"^{vo}$")))
+        .Should()
+        .Inherit(typeof(ValueObject))
+        .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ValueObjects_Should_BeImmutable()
+    {
+        var valueObjectTypes = Types.InAssembly(DomainAssembly)
+        .That()
+        .Inherit(typeof(ValueObject))
+        .GetTypes();
+
+        foreach (var type in valueObjectTypes)
+        {
+            var writableProperties = type.GetProperties()
+                .Where(p => p.CanWrite && p.SetMethod?.IsPublic == true)
+                .ToList();
+
+            writableProperties.Should().BeEmpty($"{type.Name} should have only readonly properties");
+        }
+    }
+
+    [Fact]
+    public void ValueObjects_Should_ImplementGetAtomicValues()
+    {
+        var valueObjectTypes = Types.InAssembly(DomainAssembly)
+        .That()
+        .Inherit(typeof(ValueObject))
+        .GetTypes();
+
+        foreach (var type in valueObjectTypes)
+        {
+            var method = type.GetMethod("GetAtomicValues");
+            method.Should().NotBeNull($"{type.Name} should implement GetAtomicValues method");
+            method!.IsAbstract.Should().BeFalse($"{type.Name} should implement GetAtomicValues method (not abstract)");
+        }
+    }
+
+    [Fact]
+    public void RepositoryInterfaces_Should_InheritFromIBaseRepository()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+        .That()
+        .AreInterfaces()
+        .And().HaveNameEndingWith("Repository")
+        .And().DoNotHaveName("IBaseRepository")
+        .Should()
+        .ImplementInterface(typeof(IBaseRepository<,>))
+        .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void RepositoryInterfaces_Should_BeInCorrectNamespace()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+        .That()
+        .AreInterfaces()
+        .And().HaveNameEndingWith("Repository")
+        .And().DoNotHaveName("IBaseRepository")
+        .Should()
+        .ResideInNamespaceMatching("PetBoarding_Domain\\.[A-Za-z]+$")
+        .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Domain_Should_NotHaveRepositoryImplementations()
+    {
+        var repositoryImplementations = Types.InAssembly(DomainAssembly)
+        .That()
+        .AreClasses()
+        .And().HaveNameEndingWith("Repository")
+        .GetTypes();
+
+        repositoryImplementations.Should().BeEmpty("Domain should not contain repository implementations");
+    }
+
+    [Fact]
+    public void EntityIdentifiers_Should_InheritFromEntityIdentifier()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+        .That()
+        .HaveNameEndingWith("Id")
+        .And().AreNotAbstract()
+        .And().DoNotHaveName("EntityIdentifier")
+        .Should()
+        .Inherit(typeof(EntityIdentifier))
+        .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void EntityIdentifiers_Should_FollowNamingPattern()
+    {
+        var entityIdentifierTypes = Types.InAssembly(DomainAssembly)
+        .That()
+        .Inherit(typeof(EntityIdentifier))
+        .And().DoNotHaveName("EntityIdentifier")
+        .GetTypes();
+
+        foreach (var type in entityIdentifierTypes)
+        {
+            // Vérifie que le nom se termine par "Id" et commence par une majuscule
+            type.Name.Should().EndWith("Id", $"{type.Name} should end with 'Id'");
+            type.Name.Should().MatchRegex("^[A-Z][a-zA-Z]*Id$", $"{type.Name} should follow PascalCase naming pattern ending with 'Id'");
+            
+            // Vérifie que c'est un record
+            type.IsValueType.Should().BeFalse($"{type.Name} should be a record (reference type)");
+            var baseType = type.BaseType;
+            baseType.Should().Be(typeof(EntityIdentifier), $"{type.Name} should directly inherit from EntityIdentifier");
+        }
+    }    
 }
--- Core_PetBoarding_Backend/Tests/ArchitectureTests/LayerTests.cs
+++ Core_PetBoarding_Backend/Tests/ArchitectureTests/LayerTests.cs
@@ -5,14 +5,25 @@ using FluentAssertions;
 using NetArchTest.Rules;
 
 using PetBoarding_Domain.Abstractions;
+using PetBoarding_Application.Users.GetAllUsers;
+using PetBoarding_Infrastructure.Caching;
+using PetBoarding_Persistence;
+using PetBoarding_Api.Mappers.Users;
 
 namespace ArchitectureTests;
 
 public class LayerTests
 {
     private static readonly Assembly DomainAssembly = typeof(Entity<>).Assembly;
+    private static readonly Assembly ApplicationAssembly = typeof(GetAllUsersQueryHandler).Assembly;
+    private static readonly Assembly InfrastructureAssembly = typeof(MemcachedService).Assembly;
+    private static readonly Assembly PersistenceAssembly = typeof(UnitOfWork).Assembly;
+    private static readonly Assembly ApiAssembly = typeof(UserResponseMapper).Assembly;
+
     private static readonly string ApplicationAssemblyName = "PetBoarding_Application";
     private static readonly string InfrastructureAssemblyName = "PetBoarding_Infrastructure";
+    private static readonly string PersistenceAssemblyName = "PetBoarding_Persistence";
+    private static readonly string ApiAssemblyName = "PetBoarding_Api";
 
     [Fact]
     public void Domain_Should_NotHaveDependencyOnApplication()
@@ -37,11 +48,248 @@ public class LayerTests
     }
 
     [Fact]
-    public void Infrastructure_Should_NotHaveDependencyOnApplication()
+    public void Domain_Should_NotHaveDependencyOnPersistence()
     {
         var result = Types.InAssembly(DomainAssembly)
             .Should()
+            .NotHaveDependencyOn(PersistenceAssemblyName)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Domain_Should_NotHaveDependencyOnApi()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApiAssemblyName)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_OnlyDependOnDomain()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOnAny(InfrastructureAssemblyName, PersistenceAssemblyName, ApiAssemblyName)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_NotHaveDependencyOnInfrastructure()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
             .NotHaveDependencyOn(InfrastructureAssemblyName)
+            .GetResult();
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Infrastructure_Should_NotHaveDependencyOnPersistence()
+    {
+        var result = Types.InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(PersistenceAssemblyName)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Infrastructure_Should_NotHaveDependencyOnApi()
+    {
+        var result = Types.InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApiAssemblyName)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Persistence_Should_OnlyDependOnDomainAndApplication()
+    {
+        var result = Types.InAssembly(PersistenceAssembly)
+            .Should()
+            .NotHaveDependencyOnAny(InfrastructureAssemblyName, ApiAssemblyName)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Domain_Should_NotHaveExternalDependencies()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOnAny(
+                "System.Net",
+                "System.Net.Http",
+                "Microsoft.EntityFrameworkCore",
+                "Microsoft.AspNetCore",
+                "System.Data",
+                "Npgsql")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Entities_Should_InheritFromEntityBase()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That()
+            .ResideInNamespace("PetBoarding_Domain.Entities")
+            .And()
+            .AreNotAbstract()
+            .Should()
+            .Inherit(typeof(Entity<>))
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ValueObjects_Should_BeInValueObjectsNamespace()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That()
+            .HaveNameEndingWith("Id")
+            .Or()
+            .HaveNameEndingWith("Email")
+            .Should()
+            .ResideInNamespaceStartingWith("PetBoarding_Domain")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void CommandHandlers_Should_FollowNamingConvention()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .HaveNameEndingWith("CommandHandler")
+            .Should()
+            .BeClasses()
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void QueryHandlers_Should_FollowNamingConvention()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .HaveNameEndingWith("QueryHandler")
+            .Should()
+            .BeClasses()
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void RepositoryInterfaces_Should_BeInDomain()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That()
+            .HaveNameEndingWith("Repository")
+            .And()
+            .AreInterfaces()
+            .Should()
+            .ResideInNamespaceStartingWith("PetBoarding_Domain")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void RepositoryImplementations_Should_BeInPersistence()
+    {
+        var result = Types.InAssembly(PersistenceAssembly)
+            .That()
+            .HaveNameEndingWith("Repository")
+            .And()
+            .AreNotInterfaces()
+            .Should()
+            .ResideInNamespace("PetBoarding_Persistence.Repositories")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Commands_Should_BeInApplication()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .HaveNameEndingWith("Command")
+            .Should()
+            .ResideInNamespaceStartingWith("PetBoarding_Application")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Queries_Should_BeInApplication()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .HaveNameEndingWith("Query")
+            .Should()
+            .ResideInNamespaceStartingWith("PetBoarding_Application")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Endpoints_Should_BeInApi()
+    {
+        var result = Types.InAssembly(ApiAssembly)
+            .That()
+            .HaveNameEndingWith("Endpoints")
+            .Should()
+            .ResideInNamespaceStartingWith("PetBoarding_Api.Endpoints")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Commands_Should_BeClasses()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .HaveNameEndingWith("Command")
+            .And()
+            .AreNotInterfaces()
+            .Should()
+            .BeClasses()
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Queries_Should_BeClasses()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .HaveNameEndingWith("Query")
+            .And()
+            .AreNotInterfaces()
+            .Should()
+            .BeClasses()
             .GetResult();
 
         result.IsSuccessful.Should().BeTrue();
